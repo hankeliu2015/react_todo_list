86539b87bf658824b4673562681d6d9916268904
github username <github email address>
2019-09-28 15:02:29 -0400

    add skeleton of form on todonew component for creating new tasks

--- src/containers/todonew.js
+++ src/containers/todonew.js
@@ -2,10 +2,28 @@ import React, { Component } from 'react';
 
 class TodoNew extends Component {
 
+  state = {
+    content: '',
+    content1: '',
+    content2: '',
+    completed: false
+  }
+
   render() {
+    console.log(this.state)
+
+
     return (
       <div>
         <h3>Add a new task </h3>
+
+        <form>
+          <label>Content :</label><input type = "text" name='content' value={this.state.content}/><br/>
+          <label>Content1:</label><input type = "text" name='content1' value={this.state.content1}/><br/>
+          <label>Content2:</label><input type = "text" name='content2' value={this.state.content2}/><br/>
+          <input type='submit' value='Add Todo' />
+
+        </form>
       </div>
     )
   }
