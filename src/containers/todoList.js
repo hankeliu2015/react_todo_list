324ad56933b1b6c8b3eddda17c7d060803a4b1e8
github username <github email address>
2019-09-28 13:29:07 -0400

    add varable to store the props todo array iteration returned content"

--- src/containers/todoList.js
+++ src/containers/todoList.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 
 class TodoList extends Component {
   render() {
+    let list = this.props.todos.map((todo, index) => <ToDo key={index+1} content={todo.content} completed={todo.completed} index={index}/>)
+
   return (
-    <h3>To Do List </h3>
-
-
+    <ul>{list}</ul>
   )}
 }
 
