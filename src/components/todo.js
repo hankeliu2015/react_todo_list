6380ac2c52fc19607b289ad0cb453404d4eedc1c
github username <github email address>
2019-10-05 22:08:42 -0400

    add link to todolist items to render todoshow component

--- src/components/todo.js
+++ src/components/todo.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const ToDo = (props) => {
   return (
     <li>
-      content: {props.content}; completed: {props.completed ? 'yet' : 'no'}; 
+      content: <Link to={`/todos/${props.index}`}>{props.content}</Link>; completed: {props.completed ? 'yes' : 'no'};
     </li>
   )
 }
